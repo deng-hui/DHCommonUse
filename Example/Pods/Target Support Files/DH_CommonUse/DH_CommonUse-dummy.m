bc5debfc5fee7e2a3390e820653bb2273c6647ce
王登辉 <wangdenghui@douyu.tv>
2018-11-30 15:55:57 +0800

    init

---
+++ Example/Pods/Target Support Files/DH_CommonUse/DH_CommonUse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DH_CommonUse : NSObject
+@end
+@implementation PodsDummy_DH_CommonUse
+@end
---
+++ Example/Pods/Target Support Files/Pods-DH_CommonUse_Tests/Pods-DH_CommonUse_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DH_CommonUse_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DH_CommonUse_Tests
+@end
