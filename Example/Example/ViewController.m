cab9e952dd398d72a1af348dd753784776a02fde
王登辉 <wangdenghui@douyu.tv>
2019-12-05 17:54:22 +0800

    添加列表

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -1,23 +1,75 @@
 //
 //  ViewController.m
-//  Example
+//  DHUtils
 //
-//  Created by wangdenghui on 2018/11/30.
-//  Copyright © 2018年 王登辉. All rights reserved.
+//  Created by 王登辉 on 12/05/2019.
+//  Copyright (c) 2019 王登辉. All rights reserved.
 //
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property(nonatomic, strong) UITableView *tableView;
+
+@property(nonatomic, strong) NSArray *dataArray;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _dataArray = @[@"push",
+                   @"present"];
+    
+    [self configureSubviews];
 }
 
+#pragma mark - views
+- (void)configureSubviews {
+    self.tableView.frame = self.view.bounds;
+    [self.view addSubview:self.tableView];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _dataArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    NSString *title = _dataArray[indexPath.item];
+    cell.textLabel.text = title;
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *title = _dataArray[indexPath.item];
+    if ([title isEqualToString:@"push"]) {
+        UIViewController *vc = [[UIViewController alloc] init];
+        [self.navigationController pushViewController:vc animated:YES];
+    } else if ([title isEqualToString:@"present"]) {
+        UIViewController *vc = [[UIViewController alloc] init];
+        [self presentViewController:vc animated:YES completion:nil];
+    }
+}
+
+#pragma mark - getter
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] init];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    }
+    return _tableView;
+}
 
 @end
